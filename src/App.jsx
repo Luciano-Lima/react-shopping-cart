afe024cee2dde31b8d558847942593644a6d32a5
Luciano Lima <luciano_lima@Lucianos-MacBook-Pro.local>
2025-12-01 10:55:36 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+export default function App() {
+
+    return (
+        <div className="app-header">
+          <h1>Coffee Shop</h1>
+          <p>A moder shopping cart</p>
+          <h2>Products</h2>
+        </div>
+    )
+}
---
+++ src/data/products.js
@@ -0,0 +1,45 @@
+export const products = [
+    {
+      id: "coffee-1",
+      name: "Brazilian Coffee Beans",
+      price: 12.99,
+      image:
+        "https://images.pexels.com/photos/585750/pexels-photo-585750.jpeg",
+    },
+    {
+      id: "mug-1",
+      name: "Ceramic Coffee Mug",
+      price: 8.5,
+      image:
+        "https://images.pexels.com/photos/704813/pexels-photo-704813.jpeg",
+    },
+    {
+      id: "machine-1",
+      name: "Mini Coffee Grinder",
+      price: 149.0,
+      image:
+        "https://images.pexels.com/photos/373888/pexels-photo-373888.jpeg",
+    },
+    {
+      id: "coffe-2",
+      name: "Arabic coffee Beans",
+      price: 34.90,
+      image:
+        "https://images.pexels.com/photos/27777797/pexels-photo-27777797.jpeg",
+    },
+    {
+      id: "coffe-3",
+      name: "Double Expresso",
+      price: 4.90,
+      image:
+        "https://images.pexels.com/photos/20768096/pexels-photo-20768096.jpeg",
+    },
+    {
+      id: "coffe-4",
+      name: "Boon Coffe Machine",
+      price: 4.90,
+      image:
+        "https://images.pexels.com/photos/33094652/pexels-photo-33094652.jpeg",
+    },
+  ];
+  
