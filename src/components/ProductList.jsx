7f1ecfdcb6b3628b2a5f02a1dc1089a7856bb19e
Luciano Lima <luciano_lima@Lucianos-MacBook-Pro.local>
2025-12-05 18:44:46 +0000

    Adding ProductList component to handle cart

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,13 @@
 import ProductCard from "./ProductCard";
 
-export default function ProductList({products}) {
+export default function ProductList({products, onAddToCart}) {
     return (
         <div className="product-grid">
             {products.map((product) => (
                 <ProductCard
                     key={product.id}
                     product={product}
+                    onAddToCart={onAddToCart}
                 />
             ))}
         </div>
